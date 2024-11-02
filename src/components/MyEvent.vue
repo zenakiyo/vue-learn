09082369361de163215bd3d8d90cf2be9acf3e11
zenakiyo <zenakiyo@gmail.com>
2024-11-03 01:31:52 +0530

    work event

---
+++ src/components/MyEvent.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {ref} from 'vue'
+let count = ref(0);
+</script>
+
+<template>
+  <h1>count: {{ count }}</h1>
+  <button @click='count --'>Decremetn</button>
+  <button @click='count ++'>Increment</button>
+</template>
+<style>
+</style>
